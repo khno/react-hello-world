afda7750db7eeabaa2173e1a4824663a9b53c4e5
Allan <allan91coder@gmail.com>
2018-10-23 16:47:58 +0800

    Commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-// import React from "react";
-// import { render } from "react-dom";
+import React from "react";
+import { render } from "react-dom";
 
-// class App extends React.Component {
-//   render() {
-//     return <p> Hello React</p>;
-//   }
-// }
+class App extends React.Component {
+  render() {
+    return <p> Hello React</p>;
+  }
+}
 
-// render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
 
 console.log("test")
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,22 @@ var path = require("path");
 var BUILD_DIR = path.resolve(__dirname, "build");
 var APP_DIR = path.resolve(__dirname, "src");
 
-// console.log(BUILD_DIR, APP_DIR)
 var config = {
   entry: APP_DIR + "/index.jsx",
   output: {
     path: BUILD_DIR,
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
 
