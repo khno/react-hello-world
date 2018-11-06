a6bd75f07434517a335569a1782a2d46bf5999b8
Allan <allan91coder@gmail.com>
2018-11-06 20:26:33 +0800

    Fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,10 @@
-// import React from "react";
-// import { render } from "react-dom";
+import React from "react";
+import { render } from "react-dom";
 
-// class App extends React.Component {
-//   render() {
-//     return <p> Hello React</p>;
-//   }
-// }
+class App extends React.Component {
+  render() {
+    return <p> Hello React</p>;
+  }
+}
 
-// render(<App />, document.getElementById("app"));
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-
-ReactDOM.render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
